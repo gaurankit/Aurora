8ea4ca60416657840fc90546035b3163265bdde2
Gaur <angaur_LA@AffinionDS.Com>
2019-04-24 18:41:37 -0500

    Added the app layout and static flow.

--- Workspace/auroraui/apps/auroraui/src/app/app.component.spec.ts
+++ Workspace/auroraui/apps/auroraui/src/app/app.component.spec.ts
@@ -1,31 +1,35 @@
-import { TestBed, async } from "@angular/core/testing";
-import { AppComponent } from "./app.component";
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
+describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   }));
 
-  it("should create the app", () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'auroraui'`, () => {
+  it(`should have as title 'my-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("auroraui");
+    expect(app.title).toEqual('my-project');
   });
 
-  it("should render title in a h1 tag", () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to auroraui!"
-    );
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-project!');
   });
 });
